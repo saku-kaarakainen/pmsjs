9900f62e5432f520ec5adc7c08e158c3ed5b1b7d
Saku Kaarakainen <sakuba91@hotmail.com>
2015-12-01 19:16:03 +0200

    Added menu and restart_game to assets

--- kikkare/gameover.js
+++ kikkare/gameover.js
@@ -1,7 +1,5 @@
 var gameoverState = {
 	create: function() {
-		var winLabel   = game.add.text(80,80,"You died. Game over :(",{font:"50px Arial",fill:"#FF0000"});
-		var startLabel = game.add.text(80,game.world.height-80, "press Enter to restart",{font:"25px Arial",fill:"#FFFFFF"});
 		var enterkey   = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		    enterkey.onDown.addOnce(this.restart, this);
 	},
--- kikkare/win.js
+++ kikkare/win.js
@@ -1,7 +1,5 @@
 var winState = {
 	create: function() {
-		var winLabel   = game.add.text(80,80,"You Won! :D",{font:"50px Arial",fill:"#00FF00"});
-		var startLabel = game.add.text(80,game.world.height-80, "press Enter to restart",{font:"25px Arial",fill:"#FFFFFF"});
 		var enterkey   = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		    enterkey.onDown.addOnce(this.restart, this);
 	},
