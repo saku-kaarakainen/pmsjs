6f9e07fc4cbfa0ed38407ae44087c5711935e67d
Saku Kaarakainen <sakuba91@hotmail.com>
2015-12-06 01:59:52 +0200

    edited comments

--- kikkare/win.js
+++ kikkare/win.js
@@ -1,7 +1,5 @@
 var winState = {
 	create: function() {
-		// var enterkey   = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-		 //   enterkey.onDown.addOnce(this.restart, this);
 		 game.add.text(5, 5, "you won!", {font: "12px arial", fill:"#ffffff"});
 
 		 var callback = [gameState. newgame, gameState.menu];
